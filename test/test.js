fce0f59a267312d140c24ab18b1f2a37ffc88f10
francis <francis.paintsil@amalitech.com>
2021-12-01 16:16:12 +0000

    Test fixed

--- test/test.js
+++ test/test.js
@@ -2,13 +2,9 @@ const request = require("supertest");
 const app = require("../app");
 
 describe("GET /", () => {
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 1674986ad16608cc7a7f4db75a6dfda34a91c5b3
     it('responds with Docker Test Show!', (done)=> {
         
-        request(app).get("/").expect('Docker Test Show!', done);
+        request(app).get("/").expect('Hello World!', done);
     
     })
 });
@@ -17,13 +13,5 @@ describe("GET /", () => {
 describe("GET /greetings", () => {
     it('responds with greetings', (done)=> {
         request(app).get("/greetings").expect('Hi there', done);
-    
-<<<<<<< HEAD
-=======
-    it("responds with Hello World!", (done) => {
-        request(app).get("/").expect("Hello World!", done);
->>>>>>> 4c2ece4148982e5421be1130d3b0756026048344
-=======
->>>>>>> 1674986ad16608cc7a7f4db75a6dfda34a91c5b3
     })
 });
